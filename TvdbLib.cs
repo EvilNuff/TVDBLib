d3f85df7a4cda6ea723498dd996e5055fe81b709
Julian Møller <julian@julianmoeller.dk>
2010-12-18 16:37:25 +0100

    Initial commit

---
+++ TvdbLib.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace TVDBLib
+{
+    public class TvdbLib
+    {
+        private string _xmlMirror;
+        private string _bannerMirror;
+        private string _zipMirror;
+
+        private const int XML_MASK = 1;
+        private const int BANNER_MASK = 2;
+        private const int ZIP_MASK = 4;
+
+        private readonly string _apiKey;
+
+        private const string API_MIRRORS = "http://www.thetvdb.com/api/{0}/mirrors.xml";
+
+        public TvdbLib(string api)
+        {
+            _apiKey = api;
+
+            var search = Parse<TvdbMirrorSearchResult>(string.Format(API_MIRRORS, _apiKey));
+            var mirrors = search.Mirrors;
+
+            // Save a random mirrors, per the API specifications
+            var rnd = new Random();
+            _xmlMirror = mirrors.Where(x => (x.Typemask & XML_MASK) == 1).OrderBy(x => rnd.Next()).First().MirrorPath;
+            _bannerMirror = mirrors.Where(x => (x.Typemask & BANNER_MASK) == 1).OrderBy(x => rnd.Next()).First().MirrorPath;
+            _zipMirror = mirrors.Where(x => (x.Typemask & ZIP_MASK) == 1).OrderBy(x => rnd.Next()).First().MirrorPath;
+        }
+
+        private static T Parse<T>(string uri)
+        {
+            var wc = new WebClient();
+            var s = new XmlSerializer(typeof(T));
+            var data = wc.DownloadData(uri);
+            var xml = System.Text.Encoding.UTF8.GetString(data);
+
+            if (!xml.StartsWith("<?xml"))
+            {
+                var first = xml.IndexOf("<p>");
+                var last = xml.IndexOf("</p>");
+                var e = xml.Substring(first, last - first);
+
+                throw new Exception(e);
+            }
+
+            TextReader r = new StringReader(xml);
+            return (T)s.Deserialize(r);
+        }
+    }
+}
---
+++ TvdbMirror.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace TVDBLib
+{
+    [XmlRoot("Mirrors")]
+    class TvdbMirrorSearchResult
+    {
+        [XmlArrayAttribute("Mirror")]
+        public TvdbMirror[] Mirrors;
+    }
+
+    [XmlType("Mirror")]
+    class TvdbMirror
+    {
+        [XmlAttribute("mirrorpath")]
+        public string MirrorPath;
+
+        [XmlAttribute("typepath")]
+        public int Typemask;
+    }
+}
